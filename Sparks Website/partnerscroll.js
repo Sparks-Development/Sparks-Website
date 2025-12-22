e3a834fad85414a75877e5b08d18af417d3435bc
L0gan07 <loganwyatt.tang@gmail.com>
2025-12-21 22:42:20 -0800

    Added more files and changed various display properties in index.html. Also added new JavaScript files for scrolling effects.

---
+++ Sparks Website/partnerscroll.js
@@ -0,0 +1,13 @@
+
+const partnerSection = document.getElementById("partner");
+console.log("Scrolling to the partner section...");
+
+window.onscroll = () => {
+    scrollToPartnerSection();
+function scrollToPartnerSection() {
+    
+}
+partnerSection.onclick=()=>{
+winddow.scroll
+    bottom:0,   behavior:"smooth"
+}
